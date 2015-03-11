a5c36a3c726122d0ca25e9b7d32701f6b981da14
ToukoPouko <ToukoPouko@users.noreply.github.com>
2015-03-11 20:17:01 +0200

    11.3.2015

--- FysiikkaPeli2/FysiikkaPeli2/FysiikkaPeli2/FysiikkaPeli2.cs
+++ FysiikkaPeli2/FysiikkaPeli2/FysiikkaPeli2/FysiikkaPeli2.cs
@@ -6,14 +6,20 @@ using Jypeli.Controls;
 using Jypeli.Effects;
 using Jypeli.Widgets;
 
-public class FysiikkaPeli2 : TopDownPhysicsGame
+public class FysiikkaPeli2 : PhysicsGame
 {
     Image pelaajanKuva = LoadImage("Freddy");
     Image taustanKuva = LoadImage("taustankuva");
     Image olionKuva = LoadImage("guard");
     Image reunaKuva = LoadImage("reuna");
-    PhysicsObject freddy;
+    Image kameranKuva = LoadImage("kamera1");
+    Image kameranKuva2 = LoadImage("kameratoinen1");
+    private Image[] kameranValo = LoadImages("kamera1", "kamera2");
+    private Image[] kameranValo2 = LoadImages("kameratoinen1", "kameratoinen2");
+    PlatformCharacter freddy;
     PhysicsObject guard;
+    PhysicsObject kamera;
+    PhysicsObject kamera2;
     Timer aikaLaskuri;
     Label aikaNaytto;
 
@@ -26,8 +32,10 @@ public class FysiikkaPeli2 : TopDownPhysicsGame
 
         
 
-        AddCollisionHandler(freddy, guard, PelaajatTormaavat);
-        Gravity = 0.0;
+        AddCollisionHandler<PlatformCharacter, PhysicsObject>(freddy, guard, PelaajatTormaavat);
+        Gravity = new Vector(0.0, -800.0);
+        Camera.Follow(freddy);
+        Camera.Zoom(2.0);
         
         PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
         Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
@@ -41,6 +49,8 @@ public class FysiikkaPeli2 : TopDownPhysicsGame
         ruudut.SetTileMethod(Color.FromHexCode("56FF49"), LuoPelaaja);
         ruudut.SetTileMethod(Color.Black, LuoReunat);
         ruudut.SetTileMethod(Color.Red, LuoOlio);
+        ruudut.SetTileMethod(Color.FromHexCode("0505FF"), LuoKamerat);
+        ruudut.SetTileMethod(Color.FromHexCode("FF6A00"), LuoKamerat2);
         ruudut.SetTileMethod(Color.FromHexCode("808080"), LuoTausta);
         ruudut.Optimize(Color.FromHexCode("808080"));
         ruudut.Execute(20, 20);
@@ -50,12 +60,13 @@ public class FysiikkaPeli2 : TopDownPhysicsGame
 
     void LuoPelaaja(Vector paikka, double leveys, double korkeus)
     {
-        freddy = new PhysicsObject(40, 30);
+        freddy = new PlatformCharacter(40, 30);
         freddy.Shape = Shape.Rectangle;
         freddy.Image = pelaajanKuva;
         freddy.Position = paikka;
         freddy.Tag = "freddy";
         freddy.CollisionIgnoreGroup = 1;
+        freddy.CanRotate = false;
         freddy.Restitution = 0.0;
         freddy.IgnoresPhysicsLogics = true;
         Add(freddy);
@@ -109,13 +120,13 @@ public class FysiikkaPeli2 : TopDownPhysicsGame
     void LuoNappaimet()
     {
         Keyboard.Listen(Key.Left, ButtonState.Down,
-        LiikutaPelaajaa, null, new Vector(-200, 0));
+        LiikutaPelaajaa, null, new Vector(-240, 0));
         Keyboard.Listen(Key.Right, ButtonState.Down,
-        LiikutaPelaajaa, null, new Vector(200, 0));
+        LiikutaPelaajaa, null, new Vector(240, 0));
         Keyboard.Listen(Key.Up, ButtonState.Down,
-        LiikutaPelaajaa, null, new Vector(0, 200));
+        LiikutaPelaajaa, null, new Vector(0, 240));
         Keyboard.Listen(Key.Down, ButtonState.Down,
-        LiikutaPelaajaa, null, new Vector(0, -200));
+        LiikutaPelaajaa, null, new Vector(0, -240));
         Keyboard.Listen(Key.Up, ButtonState.Released,
         LiikutaPelaajaa, null, new Vector(0, 0));
         Keyboard.Listen(Key.Down, ButtonState.Released,
@@ -135,10 +146,37 @@ public class FysiikkaPeli2 : TopDownPhysicsGame
 
     }
 
-    void PelaajatTormaavat(PhysicsObject tormaaja, PhysicsObject kohde)
+    void PelaajatTormaavat(PlatformCharacter tormaaja, PhysicsObject kohde)
     {
         aikaLaskuri.Stop();
         aikaNaytto.Stop();
+    }
+
+    void LuoKamerat(Vector paikka, double leveys, double korkeus)
+    {
+        kamera = new PhysicsObject(60, 60);
+        kamera.Image = kameranKuva;
+        kamera.CollisionIgnoreGroup = 1;
+        kamera.Position = paikka;
+        kamera.IgnoresPhysicsLogics = true;
+        kamera.Animation = new Animation(kameranValo);
+        kamera.Animation.Start();
+        kamera.Animation.FPS = 1;
+        Add(kamera);
+
+    }
+
+    void LuoKamerat2(Vector paikka, double leveys, double korkeus)
+    {
+        kamera2 = new PhysicsObject(60, 60);
+        kamera2.Image = kameranKuva2;
+        kamera2.CollisionIgnoreGroup = 1;
+        kamera2.Position = paikka;
+        kamera2.IgnoresPhysicsLogics = true;
+        kamera2.Animation = new Animation(kameranValo2);
+        kamera2.Animation.Start();
+        kamera2.Animation.FPS = 1;
+        Add(kamera2);
     }
 }
 
